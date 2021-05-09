d875609fded49378444f65065d2585bc8f8bb793
Lam Nguyen <nguyenlam3121999@gmail.com>
2021-05-09 22:09:46 +0700

    Initialize Quote Model

---
+++ Quote.WebAPI.MongoDB/Models/Quote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Quote.WebAPI.MongoDB.Models
+{
+    public class Quote
+    {
+        public string id { get; set; }
+
+        [Required]
+        public string text { get; set; }
+
+        [Required]
+        public string author { get; set; }
+    }
+}
