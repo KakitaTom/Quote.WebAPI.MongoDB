7eb78f6e42ebcd5dd95e5c20b15f167c5c35dc7c
Lam Nguyen <nguyenlam3121999@gmail.com>
2021-05-09 23:44:54 +0700

    Configuration Api

--- Quote.WebAPI.MongoDB/Global.asax.cs
+++ Quote.WebAPI.MongoDB/Global.asax.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Quote.WebAPI.MongoDB.App_Start;
 
 namespace Quote.WebAPI.MongoDB
 {
@@ -12,6 +14,14 @@ namespace Quote.WebAPI.MongoDB
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
+                .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters
+                .Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
